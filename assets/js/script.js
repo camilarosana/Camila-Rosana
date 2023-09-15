0af56fcde416ad076cb891f288be2f3f304428c4
Camila Rosana <camilarosana95@gmail.com>
2023-09-15 15:15:13 -0300

    Mudança no layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+//menu
+
 const menuMobile = document.querySelector('.menu-mobile');
 const mobileLinks = document.querySelector('.navbar-links');
 
 menuMobile.addEventListener('click', ()=>{
     menuMobile.classList.toggle('active');
     mobileLinks.classList.toggle('active');
-})
+});
